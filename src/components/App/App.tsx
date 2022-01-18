94f77b136d6f194301e91ac1ca732b7a3946bbec
Новиков Кирилл Сергеевич <ksnovikov@tsconsulting.com>
2022-01-18 22:58:54 +0300

    added styled components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,21 @@
-import './App.scss';
-
 import React from 'react';
-import { ChessBoard } from './domain/Board/Model/ChessBoard';
+import { ThemeProvider } from 'styled-components';
+import { ChessBoard } from '../../domain/Board/Model/ChessBoard';
+import Styles, { lightTheme } from '../../styles';
 
 function App() {
   const squares = new ChessBoard().squares;
 
   return (
-    <div className="App">
+    <ThemeProvider theme={lightTheme}>
       <h1>Chess</h1>
       {squares.map(square => (
         <div key={square.cellId.value} style={{ backgroundColor: square.pieceHexColor }}>
           {square.pieceType?.value}
         </div>
       ))}
-    </div>
+      <Styles />
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles.ts
@@ -0,0 +1,33 @@
+import { normalize } from 'styled-normalize';
+import { createGlobalStyle } from 'styled-components';
+
+export type Theme = typeof lightTheme;
+
+export const lightWeb = {
+  background: {
+    primary: '#FFFAFA',
+  },
+};
+export const darkTheme = {
+  background: {
+    primary: '#888',
+  },
+};
+
+export const lightTheme = { colors: lightWeb };
+
+const Styles = createGlobalStyle`
+  ${normalize}
+
+  /* html {
+    height: 100%;
+  } */
+  body {
+    /* height: 100%; */
+    background: ${({ theme }) => theme.colors.background.primary};
+  }
+  #root {
+  }
+`;
+
+export default Styles;
